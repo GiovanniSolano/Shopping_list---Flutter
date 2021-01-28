b89e35bcc44b3d894178a7ba2aced5e05f059ccb
Giovanni Solano <giovannisolano211@gmail.com>
2021-01-27 20:22:08 -0600

    Estructura del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xff181c60),
+        primaryColorDark: Color(0xff000036),
+        primaryColorLight: Color(0xff4a438e),
+        accentColor: Color(0xffedecee),
+        // backgroundColor: Color(0xff),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
